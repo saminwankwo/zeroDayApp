fbbd1ff6127d3ceb18d2d30b566dca1440d9c175
Nwankwo Samuel <nwankwosami@gmail.com>
2024-03-04 01:28:36 +0100

    made changes to workflow floder and created db connect file

---
+++ config/db.php
@@ -0,0 +1,33 @@
+<?php
+$timezone = 'Africa/lagos';
+date_default_timezone_set($timezone);
+
+if($_SERVER['HTTP_HOST'] == "localhost"){
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname ="familyTree";
+
+} else {
+
+    $servername = "localhost";
+    $username = "zspmhrph_familyUser";
+    $password = "pMBw9d58NRgXM2f";
+    $dbname ="zspmhrph_familyTree";
+}
+
+$dsn ="mysql:host=$servername;dbname=$dbname";
+
+try {
+
+    $conn = new PDO($dsn, $username, $password);
+
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
