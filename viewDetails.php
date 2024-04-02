a7591be7e33afb1942431570f30c07eb69a03df7
Nwankwo Samuel <nwankwosami@gmail.com>
2024-04-02 01:14:47 +0100

    dfnjvndf

--- viewDetails.php
+++ viewDetails.php
@@ -65,7 +65,7 @@ if(isset($_GET['view'])){
             $.ajax({
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/urllookup?url=' + url,
-        headers: { 'X-Api-Key': 'at_bDzFMsvyHe6B6dPUGRsWZmW6lLe8o'},
+        headers: { 'X-Api-Key': '9nLDbt1XJjdrETbNd6qBdg==v5SypDjkBib7KqNu'},
         contentType: 'application/json',
         beforeSend: function() {
             // Show loading text in modal
